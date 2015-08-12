be91de8928ebd9f0e9e27192a4f10d4646682432
wolfogre <wolfogre@qq.com>
2015-08-12 16:24:02 +0800

    实验证实爬虫可行

---
+++ 爬虫/Crawler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+
+namespace WebCrawler
+{
+	class Crawler
+	{
+		string _link;
+		string _fileName;
+		public Crawler(string link, string fileName)
+		{
+			_link = link;
+			_fileName = fileName;
+		}
+
+		public void Crawl()
+		{
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(_link);
+			request.Method = WebRequestMethods.Http.Get;
+			HttpWebResponse response;
+			try
+			{
+				response = (HttpWebResponse)request.GetResponse();
+			}
+			catch (System.Net.WebException)
+			{
+				Console.WriteLine("无法连接到网站" + _link);
+				return;
+			}
+			System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream());
+			string data = reader.ReadToEnd();
+			response.Close();
+
+			FileStream output = new FileStream(_fileName, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+			output.Write(Encoding.Default.GetBytes(data),0,data.Length);
+			output.Close();
+			Console.WriteLine("成功抓取:" + _link);
+			//return true;
+		}
+	}
+}
---
+++ 爬虫/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace WebCrawler
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for(int i = -180; i <= 180; ++i)
+			{
+				DateTime dt1 = DateTime.Now;
+				Thread th = new Thread(new ThreadStart((new Crawler("https://eosweb.larc.nasa.gov/cgi-bin/sse/grid.cgi?&num=182092&lat=1&hgt=100&submit=Submit&veg=17&sitelev=&email=&p=grid_id&step=2&lon=" + i, "data\\" + i + ".txt")).Crawl));
+				th.Start();
+				th.Join();
+				DateTime dt2 = DateTime.Now;
+				TimeSpan dt = dt2 - dt1;
+				Console.WriteLine(dt);
+			}
+			
+		}
+	}
+}
