27fa749f93f4b1bbc4f7245b26afb2ae21089528
wolfogre <wolfogre@qq.com>
2015-08-13 21:32:20 +0800

    升级以前写的Extrator

---
+++ 提取信息/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 提取信息
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			提取者 t = new 提取者("input\\03,073.html", "output\\03,073.txt");
+			t.提取();
+		}
+	}
+}
