4f81ed493514098a4bb87ba826c1ec467eaec78e
wolfogre <wolfogre@qq.com>
2015-08-20 14:34:47 +0800

    尝试设计气象数据库接口

---
+++ WeatherInfo/Line.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHUPV.Weather
+{
+	public class Line
+	{
+		public bool IsEmpty { get { return _isEmpty; } }
+		bool _isEmpty;
+		string _name;
+		int _num;
+		double[] _datas = new double[12];
+
+		public Line()
+		{
+			_isEmpty = true;
+		}
+
+		public override string ToString()
+		{
+			if (_isEmpty)
+				throw new Exception("Trying to call ToString() of a empty SHUPV.Weather.Line object");
+			string result = "";
+			foreach(double it in _datas)
+			{
+				result += it.ToString() + " ";
+			}
+			return result;
+		}
+	}
+}
